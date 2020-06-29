c416c610c54a2eecbae91eb2d99877e84f4d35c8
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-06-29 17:11:18 -0400

    increase minimum Android API to 22 (Android 5.1)
    and remove extra comments from sccglue/jni/Application.mk

--- sccglue/jni/Application.mk
+++ sccglue/jni/Application.mk
@@ -1,8 +1,5 @@
 # ABI NOT SUPPORTED: armeabi
 APP_ABI := armeabi-v7a x86 x86_64 arm64-v8a
 
-# NOT SUPPORTED: Android 4.4 - only if needed
-# APP_PLATFORM := android-19
-
-# Minimum Android SDK - 5.0
-APP_PLATFORM := android-21
+# Minimum Android SDK - 5.1
+APP_PLATFORM := android-22
