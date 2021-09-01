c32ddcfdd5f0496f0a0d905d29f93ea211825d59
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-09-01 12:52:47 -0400

    fix: update SQLite to 3.36.0

--- sqlite-download.mk
+++ sqlite-download.mk
@@ -1,8 +1,8 @@
-SQLITE_AMALGAMATION := sqlite-amalgamation-3320300
+SQLITE_AMALGAMATION := sqlite-amalgamation-3360000
 
 SQLITE_ZIP := $(SQLITE_AMALGAMATION).zip
 
 # Note that this part of this URL is expected to change each year:
-SQLITE_PARENT := 2020
+SQLITE_PARENT := 2021
 
 SQLITE_ZIP_URL := https://sqlite.org/$(SQLITE_PARENT)/$(SQLITE_ZIP)
